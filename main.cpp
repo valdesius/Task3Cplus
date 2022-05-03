0409a8d289b7810fd295cf4fcf528e03f96e9338
valdesius <v.inc@inbox.ru>
2022-05-03 10:22:30 +0300

    main

--- main.cpp
+++ main.cpp
@@ -3,22 +3,34 @@
 
 using namespace std;
 
-string* getArray(int numberOfElements, string* arr)
+char* getArray(int length, char* arr)
 {
-    string* newArr = new string[numberOfElements * 2];
-    for (int i = 0; i < numberOfElements; i++)
-    {
-        *(newArr + i * 2) = *(arr + i);
-        *(newArr + (2 * i + 1)) = *(arr + i);
+    int lengthFinalArr = length / 3;
+    char *finalVec = new char[lengthFinalArr];
+    int temp = 0;
+    int rep = 2;
+    for (int i = 0; i < length; i++) {
+        *(finalVec + temp) = *(arr + i + rep);
+        rep += 2;
+        temp += 1;
 
     }
-    return newArr;
+    finalVec[lengthFinalArr] = '\0';
+    return finalVec;
+}
+
+void printArr(int lengthFinalArr, char* finalVec){
+    cout << "Final array: ";
+    for (int i = 0; i < lengthFinalArr; i++) {
+        cout << finalVec[i];
+    }
 }
 
 int main() {
     int size;
     cout << "Enter the size of array: "<<endl;
     cin >> size;
+
     char sourceArr[size];
     cout << "Enter the string: " << endl;
     cin >> sourceArr;
@@ -26,25 +38,8 @@ int main() {
     int lengthSrcArr = strlen(sourceArr);
     char *vector = sourceArr;
 
-    getArray(size, vector);
-
-    int lengthFinalArr = lengthSrcArr / 3;
-    char *finalVec = new char[lengthFinalArr];
-
-
-    int temp = 0;
-    int rep = 2;
-    for (int i = 0; i < lengthSrcArr; i++) {
-        *(finalVec + temp) = *(vector + i + rep);
-        rep += 2;
-        temp += 1;
-
-    }
-    finalVec[lengthFinalArr] = '\0';
-
-    cout << "Final array: ";
-    for (int i = 0; i < lengthFinalArr; i++) {
-        cout << finalVec[i];
-    }
+    char *finalVector = getArray(size, vector);
+    int lengthFinalArr = lengthSrcArr/3;
+    printArr(lengthFinalArr, finalVector);
     return 0;
 }
